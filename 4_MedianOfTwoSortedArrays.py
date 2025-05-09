738a7128ee32abb69d7933f951b91ddfe9bd2e2c
hdschein <73927538+hdschein@users.noreply.github.com>
2025-05-09 15:09:10 -0400

    Update 4_MedianOfTwoSortedArrays.py

--- 4_MedianOfTwoSortedArrays.py
+++ 4_MedianOfTwoSortedArrays.py
@@ -5,30 +5,22 @@ class Solution(object):
         :type nums2: List[int]
         :rtype: float
         """
-        total_length = len(nums1) + len(nums2)
-        median_ix = total_length // 2
+        m, n = len(nums1), len(nums2)
+        total = m + n
+        median_ix = total // 2
 
-        if not nums1:
-            merged_arr = nums2
-        elif not nums2:
-            merged_arr = nums1
-        elif nums1[-1] <= nums2[0]:
-            merged_arr = nums1 + nums2
-        elif nums2[-1] <= nums1[0]:
-            merged_arr = nums2 + nums1
+        ix1 = ix2 = curr = 0
+        for _ in range(median_ix + 1):
+            prev = curr
+            if ix1 < m and (ix2 >= n or nums2[ix2] > nums1[ix1]):
+                curr = nums1[ix1]
+                ix1 += 1
+            else:
+                curr = nums2[ix2]
+                ix2 += 1
+        
+        if total % 2 == 1:
+            return curr
         else:
-            merged_arr = []
-            while len(merged_arr) <= median_ix:
-                if not (nums1 and nums2):
-                    merged_arr = merged_arr + nums1 + nums2
-                elif nums1[0] <= nums2[0]:
-                    merged_arr.append(nums1.pop(0))
-                else:
-                    merged_arr.append(nums2.pop(0))
-
-        # if length is even, median is average of middle two nums
-        if total_length % 2 == 0:
-            return (merged_arr[median_ix-1] + merged_arr[median_ix]) / 2.0
-        # otherwise it is just middle number
-        else:
-            return merged_arr[median_ix]
+            return (prev + curr) / 2.0
+            
